559303462346a66c3d7ff05fb45b1cf06074e67a
Jack Liang <jack.liang.tw@pmgi.local>
2019-04-19 18:24:39 +0800

    new files

--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import 'bootstrap'
+
 import App from './App.vue'
 import router from './router'
 
+Vue.use(VueAxios, axios)
+
 Vue.config.productionTip = false
+axios.defaults.withCredentials = true;
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+router.beforeEach((to, from, next) => {
+  console.log(to, from, next)
+
+  if (to.meta.requiresAuth) {
+    const api = `${process.env.VUE_APP_APIPATH}/api/user/check`
+    axios.post(api).then((res) => {
+      console.log(res.data)
+      if (res.data.success) {
+        next()
+      } else {
+        next({
+          path: '/login'
+        })
+      }
+    })
+  } else {
+    next()
+  }
+})
--- src/router.js
+++ src/router.js
@@ -1,23 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './components/pages/login.vue'
+import Products from './components/pages/products.vue'
+import Dashboard from './components/Dashboard.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '*',
+      redirect: 'login'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/admin',
+      name: 'Dashboard',
+      component: Dashboard,
+      children: [
+        {
+          path: 'products',
+          name: 'products',
+          component: Products,
+          meta: { requiresAuth: true }
+        }
+      ]
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: { requiresAuth: true }
     }
   ]
 })
